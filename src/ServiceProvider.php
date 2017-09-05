2231a809eda296b3675723d36374a5f02150c804
Nagy Szilveszter <nagy.szilveszter@bitandpixel.hu>
2017-09-05 10:46:36 +0200

    Remove publish migration Add load migration from

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,6 +19,7 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        
 //        $this->publishes([
 //            __DIR__ . '/database/migrations' => database_path('migrations'),
 //        ], 'migrations');
